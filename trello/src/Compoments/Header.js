5643c7ec5652b85b0c719886f4880cd7df28b260
atawfique12 <abirtawfique12@outlook.com>
2019-12-13 16:35:16 -0500

    [Header] CSS Adjustments and centering plus and left sign

--- trello/src/Compoments/Header.js
+++ trello/src/Compoments/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 function Header() {
-  let trello = "< Trello"
+  let leftArrow = `<`
+  let trello = `Trello`
 
   return (
     <header>
-      <h2 className="back-button">{trello}</h2>
+      <h2 className="back-button"><span className="lessthan">&lt;</span> Trello</h2>
       <h2 className="board-name">My home todolist</h2>
-      <h2 className="add-button">+ Add Task</h2>
+      <h2 className="add-button"><span className="center">+</span> Add Task</h2>
     </header>
   )
 }
