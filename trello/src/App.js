d64cfce43a3966fd671a89e0e30e17477b0bf341
atawfique12 <abirtawfique12@outlook.com>
2019-12-14 00:42:06 -0500

    [App.js] Added ApiCall for Weather and pushed to state.

--- trello/src/App.js
+++ trello/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Components/Header'
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      apikey: "5324aa7815ff9a187f7b56bc7b82b9a0",
+      zipcode: "11432",
+      weatherData: {}
+    }
+  }
+
+  apiCall = async () => {
+    let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${this.state.zipcode},us&units=imperial&appid=${this.state.apikey}`)
+    let temp = Math.floor(res.data.main.temp)
+    this.setState({
+      weatherData: {
+        icon: res.data.weather[0].icon,
+        temp
+      }
+    })
+    console.log(this.state.weatherData)
+  }
+
+  componentDidMount() {
+    this.apiCall()
   }
 
   render() {
