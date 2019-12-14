7dba3a6062a13915e6fe5d64269a1cd406b420a6
atawfique12 <abirtawfique12@outlook.com>
2019-12-13 23:56:53 -0500

    [Header] Changed CSS/Default Font [Body] Changed gradient

--- trello/src/Compoments/Header.js
+++ trello/src/Compoments/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
       <h2 className="back-button"><span className="lessthan">&lt;</span> Trello</h2>
-      <h2 className="board-name">My home todo list</h2>
+      <h2 className="board-name">Team Tasks</h2>
       <h2 className="add-button"><span className="center">+</span> Add Task</h2>
     </header>
   )
