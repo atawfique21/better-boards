cf9ef904a0bc02904f678a4e8f015d8c5e7d3a26
atawfique12 <abirtawfique12@outlook.com>
2019-12-13 22:18:50 -0500

    [Header] Finished mock-up header, just needs functionality.

--- trello/src/Compoments/Header.js
+++ trello/src/Compoments/Header.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
 function Header() {
-  let leftArrow = `<`
-  let trello = `Trello`
-
   return (
     <header>
       <h2 className="back-button"><span className="lessthan">&lt;</span> Trello</h2>
-      <h2 className="board-name">My home todolist</h2>
+      <h2 className="board-name">My home todo list</h2>
       <h2 className="add-button"><span className="center">+</span> Add Task</h2>
     </header>
   )
