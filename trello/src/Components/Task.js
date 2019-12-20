94febef278a3973dc96f7af64501b2fe6f5cb694
atawfique12 <easternselling@gmail.com>
2019-12-19 21:00:08 -0500

    Removed comments

--- trello/src/Components/Task.js
+++ trello/src/Components/Task.js
@@ -2,21 +2,6 @@ import React from 'react'
 import DeleteTask from './DeleteTask'
 import AddDetails from './AddDetails'
 
-/*
-TODO: 
-
-1. Finish AddDetails
-  1.1. Task gets passed through prop which needs to be unpacked for AddDetails and then conditionally show the buttons. You'll need many different conditional
-  statements to figure out what buttons to push and which ones to not depending on what's already there. 
-  1.2. On button submit, in here, we need to handle the click.
-    1.2.1. In here, we'll have a state called "addNote: false". 
-      - If false, it will be normal and just render the button through AddDetails. 
-      - If true, we hide the button and instead have a input field that shows underneath all the task components where they can enter their notes. 
-  1.3. On field change, we update state noteInput!!
-  1.3 On formSubmit={props.handleSubmit} which will also send e + inputData + sentTask
-  1.4 Code already exists in App.JS that should edit the notes to what it is. 
-*/
-
 class Task extends React.Component {
   constructor(props) {
     super(props)
